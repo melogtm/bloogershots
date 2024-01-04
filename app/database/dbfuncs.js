de965941e1c5c8d332f2aa89c305d51fbd07b54b
gabrieltrigueiro <gabrielcursos@proton.me>
2024-01-04 15:46:58 -0300

    add login and register pages

---
+++ app/database/dbfuncs.js
@@ -0,0 +1,5 @@
+import query from "./db";
+
+function createUser() {
+
+};
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser"; 
+import session from 'express-session'; 
+import passport from 'passport';
 
 const app = express(); 
 const port = 3000;
@@ -10,8 +13,29 @@ app.set('views', './views');
 app.use(express.static("public")); 
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Config Sessions 
+app.use(session({
+    secret: process.env.SECRETPHRASE, 
+    resave: false,
+    saveUninitialized: false, 
+    cookie: {
+        maxAge: 3600000
+    }
+}));
+
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register"); 
 });
 
 app.listen(port, () => {
